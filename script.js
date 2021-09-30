b7484e75f7186b209fba728dd09e13f0d8c91a4f
Gregory Banks <gbanks1438@gmail.com>
2021-09-30 00:37:29 -0400

    Updated About section and formatting

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Learning to code with HTML, CSS, and JavaScript!";
+h2.textContent = "I'm learning to code with HTML, CSS, and JavaScript!";
 document.querySelector("body").appendChild(h2);
